bc8eb3654864f78f0df0f29e7052ea35d1db5c45
colin <colinrozzi@gmail.com>
2026-05-12 10:32:20 -0400

    flake: bump theater to release-20260512 (graceful TLS shutdown on close)

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     crane.url = "github:ipetkov/crane";
 
     theater = {
-      url = "github:colinrozzi/theater/release-20260510";
+      url = "github:colinrozzi/theater/release-20260512";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.rust-overlay.follows = "rust-overlay";
       inputs.crane.follows = "crane";
